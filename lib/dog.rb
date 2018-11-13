46568a0fa46875ca101afb64c55268e080e79bfd
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-13 18:19:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,14 @@ class Dog(name, breed)
     @name = name
     @breed = breed
   end 
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   def name 
     @name
   end
-  def breed=(breed)
-    @breed = breed 
+  def breed=(new_breed)
+    @breed = new_breed 
   end
   def breed
     @breed
